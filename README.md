8cf5b4471a565c567b7c91402a79b9169754fbca
Alexis Kinsella <alexis.kinsella@gmail.com>
2015-03-01 00:18:17 +0100

    Added license and script

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
 # kimsufi-availability-checker-shell
+
+Shell script to check availability of kimsufi server.
+
+## Author
+
+Alexis Kinsella ( @alexiskinsella )
+
+
+## Installation
+
+### jq ( http://stedolan.github.io/jq/ )
+
+Mac: 
+
+	brew install jq
+
+Linux:
+
+	sudo yum install jq
+	sudo apt-get install jq
+
+
+### Mandrill account ( https://mandrillapp.com/ )
+
+To send mail, you can use Mandrill. If you don't have a mandrill account, you need to create one ...
+
+
+### Make your shell script executable
+
+	chmod 744 ./kimsufi-availability.sh
+
+
+## Execute
+
+	./kimsufi-availability.sh 150sk60 rbx 0000000000000000000000 john.doe@gmail.com "John Doe" john.doe@gmail.com
+
+
+### Parameters
+
+    1 - Reference pattern (grep pattern) - Example: 150sk60
+	2 - Zone pattern (zone pattern) - Example: rbx
+	3 - Mandrill API key - Example: 0000000000000000000000
+	4 - From email - Example: john.doe@gmail.com
+	5 - From name - Example: John Doe
+	6 - To email - Example: john.doe@gmail.com
+
+### Cron your shell script
+
+Edit your crontab:
+
+	crontab -e
+
+Crontab entry:
+
+	*/15 * * * * /home/<user>/kimsufi-availability.sh 150sk60 rbx 0000000000000000000000 john.doe@gmail.com "John Doe" john.doe@gmail.com >> /var/log/kimsufi-availability.log 2>&1
---
+++ kimsufi-availability.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+#Copyright (c) 2015 Alexis Kinsella <alexis.kinsella@gmail.com>
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy of 
+#this software and associated documentation files (the "Software"), to deal in the 
+#Software without restriction, including without limitation the rights to use, copy, 
+#modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+#and to permit persons to whom the Software is furnished to do so, subject to the 
+#following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all copies 
+#or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+#INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+#PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+#FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
+#ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+if [[ "3" != "3" ]] &&  [[ "3" != "3" ]]; then echo "yes"; else echo "no"; fi
+
+DATA=$(curl https://ws.ovh.com/dedicated/r2/ws.dispatcher/getAvailability2)
+
+
+echo "Searching for OVH references and zone availabilities ..."
+REFERENCES=$(echo $DATA | jq '.answer.availability[].reference' | grep "$1")
+for REFERENCE in $REFERENCES; do
+    echo "REFERENCE: $REFERENCE"
+    ZONES=$(echo $DATA | jq '.answer.availability[] | select(.reference == '$REFERENCE').zones[].zone' | grep "$2")
+    for ZONE in $ZONES; do
+        echo "  - ZONE: $ZONE"
+        AVAILABILITY=$(echo $DATA | jq '.answer.availability[] | select(.reference == '$REFERENCE').zones[] | select(.zone == '$ZONE').availability')
+        echo "AVAILABILITY: $AVAILABILITY"
+        if [[ "$AVAILABILITY" != "\"unknown\"" ]] && [[ "$AVAILABILITY" != "\"unavailable\"" ]]; then
+            RESULTS="$RESULTS  - $REFERENCE $ZONE is available: ${AVAILABILITY}"$'\n'
+        fi
+    done
+done
+
+if [[ $RESULTS == "" ]]; then
+    echo "No Reference / zone available."
+    exit 1
+fi
+RESULTS="References / zones available : "$'\n'$RESULTS
+RESULTS=$(echo $RESULTS | sed s/\"/\'/g)
+echo "$RESULTS"
+
+key="$3"
+from_email="$4"
+reply_to="$from_email"
+from_name="$5"
+to_email="$6"
+subject="References / zones available"
+
+msg='{ "async": false, "key": "'$key'", "message": { "from_email": "'$from_email'", "from_name": "'$from_name'", "headers": { "Reply-To": "'$reply_to'" }, "return_path_domain": null, "subject": "References / zones available", "text": "'$RESULTS'", "to": [ { "email": "'$to_email'", "type": "to" } ] } }'
+echo "CURL JSON: $msg"
+results=$(curl -A 'Mandrill-Curl/1.0' -d "$msg" 'https://mandrillapp.com/api/1.0/messages/send.json' -s 2>&1);
+echo "$results" | grep "sent" -q; 
+if [ $? -ne 0 ]; then
+    echo "An error occured: $results";
+    exit 2;
+fi 
+
+exit 0
